1f1ec43a372bc159f04635d8c2fcbf58cc0e0957
Yilong Song <songyilong18@gmail.com>
2025-03-08 15:18:22 -0500

    corrections

--- train.py
+++ train.py
@@ -26,6 +26,8 @@ def main(cfg: OmegaConf):
     # resolve immediately so all the ${now:} resolvers
     # will use the same time.
     OmegaConf.resolve(cfg)
+    
+    print("Dataset used:", cfg.dataset.dataset_path)
 
     cls = hydra.utils.get_class(cfg._target_)
     workspace: BaseWorkspace = cls(cfg)
